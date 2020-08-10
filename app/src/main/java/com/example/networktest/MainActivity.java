b45de55d6be5c68425687d6a104b704da6c82c42
Yan <203676187@qq.com>
2020-08-10 15:58:47 +0800

    Initial commit

---
+++ app/src/main/java/com/example/networktest/MainActivity.java
@@ -0,0 +1,85 @@
+package com.example.networktest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    TextView responseText;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button sendRequest=(Button)findViewById(R.id.send_request);
+        responseText=(TextView)findViewById(R.id.response_text);
+        sendRequest.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.send_request){
+            sendRequestWithHttpURLConnection();
+        }
+    }
+
+    private void sendRequestWithHttpURLConnection() {
+        //开启线程来发起网络请求
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                HttpURLConnection connection=null;
+                BufferedReader reader=null;
+                try {
+                    //获取HttpURLConnection实例
+                    URL url=new URL("https://www.baidu.com");
+                    connection = (HttpURLConnection) url.openConnection();
+                    //设置HTTP请求所用的方法，GET为从服务器那里获取数据
+                    connection.setRequestMethod("GET");
+                    //连接超时和读取超时的毫秒数
+                    connection.setConnectTimeout(8000);
+                    connection.setReadTimeout(8000);
+                    //获取服务器返回的输入流
+                    InputStream in = connection.getInputStream();
+
+                    //下面对输入流进行读取
+                    reader = new BufferedReader(new InputStreamReader(in));
+                    StringBuilder response = new StringBuilder();
+                    String line;
+                    while ((line = reader.readLine()) != null){
+                        response.append(line);
+                    }
+                    showResponse(response.toString());
+
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
+
+    //Android不允许在子线程中进行UI操作
+    private void showResponse(final String response) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                //在这里进行UI操作，将结果显示到界面上
+                responseText.setText(response);
+            }
+        });
+    }
+}
